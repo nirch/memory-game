d4c196a31e03c3d44d6413df2216b26047e0db78
nirch <nirh2@yahoo.com>
2021-02-11 22:54:04 +0200

    MemoryCard initial

---
+++ src/components/MemoryCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+function MemoryCard(props) {
+    return (
+        <div className="c-memory-card">
+            
+        </div>
+    );
+}
+
+export default MemoryCard;
