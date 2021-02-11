59614bba6cb21f9cd86f53f8c7a66e7e0a3e7d57
nirch <nirh2@yahoo.com>
2021-02-11 23:23:54 +0200

    MemoryCard background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import MemoryCard from './components/MemoryCard';
+import SettingsContext from './shared/SettingsContext';
+import cardBackground from './assets/images/background-square-card.jpg'
 
 function App() {
+  const [settings, setSettings] = useState({
+    cardBackground
+  })
+
+
   return (
-    <div className="App">
-      <MemoryCard/>
-    </div>
+    <SettingsContext.Provider value={settings}>
+      <div className="App">
+        <MemoryCard/>
+      </div>
+    </SettingsContext.Provider>
   );
 }
 
--- src/components/MemoryCard/index.js
+++ src/components/MemoryCard/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import SettingsContext from '../../shared/SettingsContext';
 import './style.css'
 
 function MemoryCard(props) {
+    const {cardBackground} = useContext(SettingsContext);
+
     return (
         <div className="c-memory-card">
-            
+            <img src={cardBackground} alt="Memory Card Background"/>
         </div>
     );
 }
