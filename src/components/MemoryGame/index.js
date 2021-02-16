755ec92fc513f643056dc90cd5f6d7590d42a784
nirch <nirh2@yahoo.com>
2021-02-16 15:15:01 +0200

    game logic.

--- src/components/MemoryGame/index.js
+++ src/components/MemoryGame/index.js
@@ -8,7 +8,6 @@ function MemoryGame(props) {
     const {cardImgs} = props;
     const [cards, setCards] = useState([]);
 
-
     useEffect(() => {
         // creating 2 cards of the same image. then shuffling the cards
         const cards = [];
@@ -20,13 +19,57 @@ function MemoryGame(props) {
         setCards(cards);
     }, [cardImgs])
 
+    useEffect(() => {
+        const faceUpNotMached = cards.filter(card => card.faceUp && !card.matched);
+        if (faceUpNotMached.length > 1) {
+            setTimeout(() => {
+                const cardsClone = [...cards];
+                faceUpNotMached.forEach(card => {
+                    const index = cards.indexOf(card);
+                    const cardClone = {...cards[index]};
+                    cardClone.faceUp = false;
+                    cardsClone[index] = cardClone;
+                })
+                setCards(cardsClone);
+            }, 2000);
+        }
+    }, [cards])
 
     function cardClicked(index) {
-        const cardsClone = [...cards];
-        const cardClone = {...cards[index]};
-        cardClone.faceUp = !cardClone.faceUp;
-        cardsClone[index] = cardClone;
-        setCards(cardsClone);
+        const faceUpNotMached = cards.filter(card => card.faceUp && !card.matched);
+
+        if (faceUpNotMached.length > 1) {
+            // do nothing
+        } else if (cards[index].matched) {
+            // do nothing
+        } else {
+            const cardsClone = [...cards];
+            const cardClone = {...cards[index]};
+            cardClone.faceUp = !cardClone.faceUp;
+            cardsClone[index] = cardClone;
+
+            if (faceUpNotMached.length === 1) {
+                // this is the 2nd card checking for a match
+                if (cardClone.id === faceUpNotMached[0].id) {
+                    cardClone.matched = true;
+                    const firstCardIndex = cards.indexOf(faceUpNotMached[0]);
+                    const firstCardClone = {...cards[firstCardIndex]};
+                    firstCardClone.matched = true;
+                    cardsClone[firstCardIndex] = firstCardClone;
+                }
+            }
+
+            setCards(cardsClone);
+        }
+
+        // checking if there is a match
+
+
+        // const cardsClone = [...cards];
+        // const cardClone = {...cards[index]};
+        // cardClone.faceUp = !cardClone.faceUp;
+        // cardsClone[index] = cardClone;
+        // setCards(cardsClone);
     }
 
     const cardsView = cards.map((card, index) => 
